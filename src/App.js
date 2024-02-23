57285256eb2fc7a04655ab27ab314ea177a6cb46
AishwaryaSubash <riya129ash@gmail.com>
2024-02-23 16:06:44 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <p>Master changes</p>
       <p>Feature changes</p>
     </div>
   );
